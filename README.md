29ad55eeeb8cf45b9cdf7ddc5d7e845d7dfef8fe
nguyen451 <uyenbich942006@gmail.com>
2024-08-17 15:04:40 +0700

    Layout changing

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # final-project
+## Ques in layout (alert) - finance
+ I don't know why my finance web doesn't have any alert even in my layout.html has one. What is the problem with the function get_flashed_message()? It from jinja, is it?
+
+if you want to read like adocument: delete these '''
+'''
+    {% if get_flashed_messages() %}
+        <header>
+            <div class="alert alert-primary mb-0 text-center" role="alert">
+                {{ get_flashed_messages() | join(" ") }}
+            </div>
+        </header>
+    {% endif %}
+
+## Ques in check html (i love validator) - finance
+I'm not really understand this :
+        <footer class="mb-5">
+            <form action="https://validator.w3.org/check" class="text-center" enctype="multipart/form-data" method="post" target="_blank">
+                <input name="doctype" type="hidden" value="HTML5">
+                <input name="fragment" type="hidden">
+                <input alt="Validate" src="/static/I_heart_validator.png" type="image"> <!-- https://validator.w3.org/ -->
+            </form>
+            <script>
+                document.addEventListener('DOMContentLoaded', function() {
+                    // Adapted from https://stackoverflow.com/a/10162353
+                    const html = '<!DOCTYPE ' +
+                    document.doctype.name +
+                    (document.doctype.publicId ? ' PUBLIC "' + document.doctype.publicId + '"' : '') +
+                    (!document.doctype.publicId && document.doctype.systemId ? ' SYSTEM' : '') +
+                    (document.doctype.systemId ? ' "' + document.doctype.systemId + '"' : '') +
+                    '>\n' + document.documentElement.outerHTML;
+                    document.querySelector('form[action="https://validator.w3.org/check"] > input[name="fragment"]').value = html;
+                });
+            </script>
+        </footer>
+
--- app.py
+++ app.py
@@ -6,3 +6,89 @@ from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import apology, login_required
+
+# Configure application
+app = Flask(__name__)
+
+# Custom filter: no need for usd filter
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///mbti.db")
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/")
+@login_required
+def index():
+    return render_template("index.html")
+
+
+@app.route("/mbti")
+@login_required
+def mbti():
+    return apology("Todo", 404)
+
+
+@app.route("/major")
+@login_required
+def major():
+    return apology("Todo", 404)
+
+
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # POST
+    if request.method == "POST":
+
+        # Exist user name and pw
+        if not request.form.get("username") or not request.form.get("password"):
+            return apology("Must provide username and password", 400)
+        
+        # Query database for username
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
+
+        # Ensure usename exists and password is correct
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
+            return apology("invalid username and/or password", 400)
+        
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    """Log user out"""
+
+    # Logout
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
