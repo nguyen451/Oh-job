6de68b0996d928d97c7d847a0afb04f2375e6e8f
nguyen451 <uyenbich942006@gmail.com>
2024-12-24 13:20:58 +0000

    last change

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology, login_required
+from helpers import login_required
 
 # Configure application
 app = Flask(__name__)
